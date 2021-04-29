43a50a05ee2147164a68e36cd3b75002a51e0852
maryplank <kaleidoscopesland@hotmail.com>
2021-04-29 11:52:48 -0300

    created get by country, put and delete methods (last two for developing purposes)

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,13 +1,55 @@
+const { response } = require("express");
 const Campaign = require("../models/Campaign");
 
 // @desc    Get best campaign according to country
 // @route   GET /api/v1/campaigns/:country
 // @access  Private
 
-exports.getCampaigns = (req, res, next) => {
-  res.status(200).json({
+exports.getCampaigns = async (req, res, next) => {
+  try {
+    const campaigns = await Campaign.find({ country: req.params.country });
+
+    res.status(200).json({
+      success: true,
+      count: campaigns.length,
+      data: campaigns,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
+
+// @desc    Update campaign
+// PUT /api/v1/campaigns/:id
+// @access Private
+
+exports.updateCampaign = async (req, res, next) => {
+  const campaign = await Campaign.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!campaign) {
+    return res.status(400).json({ success: false });
+  }
+
+  return res.status(200).json({ success: true, data: campaign });
+};
+
+// @desc    Delete campaign
+// PUT /api/v1/campaigns/:id
+// @access Private
+
+exports.deleteCampaign = async (req, res, next) => {
+  const campaign = await Campaign.findByIdAndDelete(req.params.id);
+
+  if (!campaign) {
+    return res.status(400).json({ success: false });
+  }
+
+  return res.status(200).json({
     success: true,
-    data: `Show the best campaign for ${req.params.country}`,
+    data: `Campaign with id ${req.params.id} deleted.`,
   });
 };
 
@@ -16,10 +58,14 @@ exports.getCampaigns = (req, res, next) => {
 // @access  Private
 
 exports.createCampaign = async (req, res, next) => {
-  const campaign = await Campaign.create(req.body);
+  try {
+    const campaign = await Campaign.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    data: campaign,
-  });
+    res.status(201).json({
+      success: true,
+      data: campaign,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
-const { getCampaigns, createCampaign } = require("../controllers/campaigns");
+const {
+  getCampaigns,
+  createCampaign,
+  updateCampaign,
+  deleteCampaign,
+} = require("../controllers/campaigns");
 
 const router = express.Router();
 
 router.route("/:country").get(getCampaigns);
 
 router.route("/").post(createCampaign);
+
+router.route("/:id").put(updateCampaign).delete(deleteCampaign);
 
 module.exports = router;
