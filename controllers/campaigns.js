d416bd45170009f2662150a45ee4fda822386d94
maryplank <kaleidoscopesland@hotmail.com>
2021-04-29 14:31:39 -0300

    get request returns campaign with highest bid

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,18 +1,20 @@
-const { response } = require("express");
 const Campaign = require("../models/Campaign");
 
 // @desc    Get best campaign according to country
 // @route   GET /api/v1/campaigns/:country
 // @access  Private
 
-exports.getCampaigns = async (req, res, next) => {
+exports.getCampaigns = async (req, res) => {
   try {
     const campaigns = await Campaign.find({ country: req.params.country });
+    const highestBid = campaigns.reduce(
+      (max, campaign) => (max && max.bid > campaign.bid ? max : campaign),
+      null
+    );
 
     res.status(200).json({
       success: true,
-      count: campaigns.length,
-      data: campaigns,
+      data: highestBid,
     });
   } catch (err) {
     res.status(400).json({ success: false });
@@ -23,7 +25,7 @@ exports.getCampaigns = async (req, res, next) => {
 // PUT /api/v1/campaigns/:id
 // @access Private
 
-exports.updateCampaign = async (req, res, next) => {
+exports.updateCampaign = async (req, res) => {
   const campaign = await Campaign.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -40,7 +42,7 @@ exports.updateCampaign = async (req, res, next) => {
 // PUT /api/v1/campaigns/:id
 // @access Private
 
-exports.deleteCampaign = async (req, res, next) => {
+exports.deleteCampaign = async (req, res) => {
   const campaign = await Campaign.findByIdAndDelete(req.params.id);
 
   if (!campaign) {
@@ -57,7 +59,7 @@ exports.deleteCampaign = async (req, res, next) => {
 // @route   POST /api/v1/campaigns
 // @access  Private
 
-exports.createCampaign = async (req, res, next) => {
+exports.createCampaign = async (req, res) => {
   try {
     const campaign = await Campaign.create(req.body);
 
