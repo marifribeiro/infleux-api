89063b58ce07998db3e499ca45031bd0a2aadf91
maryplank <kaleidoscopesland@hotmail.com>
2021-04-30 19:06:29 -0300

    fixing CORS errors

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/v1/campaigns", campaigns);
 
 const PORT = process.env.PORT || 5000;
